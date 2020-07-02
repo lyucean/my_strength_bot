76f396bc3ee5e68b84eb7292f27b45c23afebab9
lyucean <lyucean@gmail.com>
2020-07-02 20:13:00 +0300

    + menu + Help + FAQ

---
+++ command/Help.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace msb\command;
+
+use Telegram;
+
+class Help
+{
+    private Telegram $telegram;
+    private int $chat_id;
+
+    public function __construct($telegram)
+    {
+        $this->telegram = $telegram;
+        $this->chat_id = $this->telegram->ChatID();
+    }
+
+    public function index()
+    {
+        $this->telegram->sendMessage(
+            [
+                'chat_id' => $this->chat_id,
+                'text' => 'Если вам необходима тех. поддержка, вы может написать мне @lyucean'
+            ]
+        );
+    }
+}
---
+++ command/Menu.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace msb\command;
+
+use Telegram;
+
+class Menu
+{
+    private Telegram $telegram;
+    private int $chat_id;
+
+    public function __construct($telegram)
+    {
+        $this->telegram = $telegram;
+        $this->chat_id = $this->telegram->ChatID();
+    }
+
+    public function index()
+    {
+        $option = [
+            [
+                $this->telegram->buildInlineKeyBoardButton(
+                    'Каталог сообщений',
+                    $url = '',
+                    '/catalog'
+                )
+            ],
+            [
+                $this->telegram->buildInlineKeyBoardButton(
+                    'Настройки',
+                    $url = '',
+                    '/setting'
+                )
+            ],
+            [
+                $this->telegram->buildInlineKeyBoardButton(
+                    'Как это работает',
+                    $url = '',
+                    '/faq'
+                )
+            ],
+            [
+                $this->telegram->buildInlineKeyBoardButton(
+                    'Тех.поддержка',
+                    $url = '',
+                    '/help'
+                )
+            ],
+        ];
+
+        $this->telegram->sendMessage(
+            [
+                'chat_id' => $this->chat_id,
+                'reply_markup' => $this->telegram->buildInlineKeyBoard($option),
+                'text' => 'Меню'
+            ]
+        );
+    }
+}
