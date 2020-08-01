fc6c9d817deda0d7a2ae54366bef119ee5708269
lyucean <lyucean@gmail.com>
2020-08-01 12:07:00 +0300

    + Sentry release

--- app/error.php
+++ app/error.php
@@ -2,8 +2,9 @@
 
 if (OC_ENV_PROD) {
     Sentry\init(
-        ['dsn' => 'https://' . SENTRY_PROJECT_KEY . '@o443919.ingest.sentry.io/' . SENTRY_ID]
+        [
+            'dsn' => 'https://' . SENTRY_PROJECT_KEY . '@o443919.ingest.sentry.io/' . SENTRY_ID,
+            'release' => PROJECT_NAME . '@' . PROJECT_VERSION,
+        ]
     );
 }
-
-
